fc7ebb5d9c07f2f4f72ab9aebd976f44d0dfde0d
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-16 11:49:25 +0100

    playing with threadpool

--- src/ExcecutorServiceRunner.java
+++ src/ExcecutorServiceRunner.java
@@ -1,18 +1,44 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class ExcecutorServiceRunner {
- public static void main(String[] args) {
-  ExecutorService executorService = Executors.newSingleThreadExecutor();
+class Task extends Thread {
 
-  executorService.execute(new Task1());
-  executorService.execute(new Thread(new Task2()));
+ private int number;
 
-  System.out.print("\nTask 3 is starting");
-  for (int i = 301; i < 400; i++) {
+ public Task(int number) {
+  this.number = number;
+ }
+
+ public void run() {
+
+  System.out.print("\nTask " + number + " is starting\n");
+  for (int i = number * 100; i < number * 100 + 100; i++) {
    System.out.print(i + " ");
   }
-  System.out.print("\nTask 3 is done");
+  System.out.print("\nTask " + number + " is done\n");
+
+ }
+}
+
+class Task2 implements Runnable {
+
+ public void run() {
+  System.out.print("\nTask 2 is starting\n");
+  for (int i = 201; i < 300; i++) {
+   System.out.print(i + " ");
+  }
+  System.out.print("\nTask 2 is done\n");
+
+ }
+}
+
+public class ExcecutorServiceRunner {
+ public static void main(String[] args) {
+  ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+  executorService.execute(new Task(1));
+  executorService.execute(new Task(2));
+  executorService.execute(new Task(4));
 
   System.out.print("\nMain method is done");
   executorService.shutdown();
