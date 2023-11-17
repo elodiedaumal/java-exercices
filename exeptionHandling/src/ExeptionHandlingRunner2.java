2511b34a88b6dc0c6f4ff92c9f814ff84148dd60
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-17 10:00:46 +0100

    handling specific exeptions

--- exeptionHandling/src/ExeptionHandlingRunner2.java
+++ exeptionHandling/src/ExeptionHandlingRunner2.java
@@ -11,11 +11,22 @@ public class ExeptionHandlingRunner2 {
 
     public static void method2() {
         try {
-            String str = null;
-            str.length();
+            // String str = null;
+            // str.length();
+            int[] i = { 1, 2 };
+            int number = i[2];
             System.out.println("Method2 ended");
-        } catch (Exception e) {
+        } catch (NullPointerException e) {
+            System.out.println("Null pointer exeption");
             e.printStackTrace();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bound exeption");
+            e.printStackTrace();
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Matched exeption");
         }
 
     }
