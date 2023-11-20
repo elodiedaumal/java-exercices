a165da4d3c7563e4e9ae0c1e90cce5d1c315f1c9
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-11-20 12:44:20 +0100

    use of concurentHashMap

--- exeptionHandling/src/Concurrency/ConcurrentMapRunner.java
+++ exeptionHandling/src/Concurrency/ConcurrentMapRunner.java
@@ -1,22 +1,16 @@
 package Concurrency;
 
-import java.util.Hashtable;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.LongAdder;
 
 public class ConcurrentMapRunner {
  public static void main(String[] args) {
 
-  Map<Character, LongAdder> occurances = new Hashtable<>();
+  ConcurrentHashMap<Character, LongAdder> occurances = new ConcurrentHashMap<>();
   String string = "ABCD ABCD ABCD";
   for (char character : string.toCharArray()) {
-   LongAdder longAdder = occurances.get(character);
-   if (longAdder == null) {
-    longAdder = new LongAdder();
+   occurances.computeIfAbsent(character, ch -> new LongAdder()).increment();
 
-   }
-   longAdder.increment();
-   occurances.put(character, longAdder);
   }
   System.out.println(occurances);
  }
